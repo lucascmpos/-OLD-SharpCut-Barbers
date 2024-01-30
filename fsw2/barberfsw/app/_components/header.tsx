98d9f765982e657c506c1dd1ce16a35c797bf62b
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-01-30 16:40:03 -0300

    feat: add barbershop item

--- fsw2/barberfsw/app/_components/header.tsx
+++ fsw2/barberfsw/app/_components/header.tsx
@@ -9,8 +9,8 @@ const Header = () => {
       <CardContent className="p-5 justify-between flex flex-row">
         <div className="flex flex-row">
           <Image src="/scissor.png" alt="LC Barbers" height={22} width={38} />
-          <h1 className="font-bold text-4xl flex">
-            <p className="font-extrabold text-red-800 mr-2">LC </p>BARBERS
+          <h1 className="font-bold text-4xl flex uppercase">
+            <p className="font-extrabold text-red-800 mr-2">LC </p>Barbers
           </h1>
         </div>
         <Button variant="outline" size="icon">
