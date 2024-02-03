012f3d0a7cdf89190e211ca5994d0eb726d5ef9a
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-02-03 10:15:03 -0300

    fix: ensure authOptions are exported from lib folder

--- fsw2/barberfsw/app/barbershops/[id]/page.tsx
+++ fsw2/barberfsw/app/barbershops/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -14,6 +13,7 @@ interface BarbershopDetailsPageProps {
 const BarbershopDetailsPage = async ({
   params,
 }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
   if (!params.id) {
     // TODO: redirecionar para home page
     return null;
